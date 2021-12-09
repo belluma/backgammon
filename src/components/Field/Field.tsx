c26899f0c64297d521070f36b7223f18a4da5a8f
ben <benedikt.maciejczyk@gmx.de>
2021-12-09 18:09:06 +0100

    refactor chip

--- src/components/Field/Field.tsx
+++ src/components/Field/Field.tsx
@@ -3,44 +3,31 @@ import styles from "./Field.module.css";
 import Chip from "../Chip/Chip";
 
 type Props = {
-  className?: string;
-  chips: number[];
-  top: boolean;
-  selected?: boolean;
-  selectField: () => void;
+    className?: string;
+    chips: number[];
+    top: boolean;
+    selected?: boolean;
+    selectField: () => void;
 };
 
 const Field: React.FC<Props> & React.HTMLAttributes<HTMLDivElement> = (
-  props
-) => {
-  const chips = [];
-  const amount = props.chips.reduce((a, b) => a + b);
-  for (let i = 0; i < amount; i++) {
-    const classNames = ["chip"];
-    props.chips[0] > 0
-      ? classNames.push("chip-white")
-      : classNames.push("chip-black");
-    if (props.selected && i === amount - 1) classNames.push("selected");
-    chips.push(
-      <Chip
-        top={props.top}
-        position={i}
-        className={classNames.join(" ")}
-        key={i}
-      />
+    {className, chips, top, selected, selectField}: Props) => {
+    const amount = chips.reduce((a, b) => a + b);
+    const getClassName = (i: number) => {
+        return ["chip", chips[0] > 0 ? "chip-white" : "chip-black", selected && i === amount - 1 && "selected"].join(' ');
+    }
+    const chipStack = [...Array(amount)].map((_, i) => <Chip top={top} position={i} className={getClassName(i)}
+                                                             key={i}/>)
+    return (
+        <section
+            className={styles.Field}
+            data-testid="Field"
+            onClick={() => selectField()}
+        >
+            <div className={className}/>
+            {chipStack}
+        </section>
     );
-  }
-
-  return (
-    <section
-      className={styles.Field}
-      data-testid="Field"
-      onClick={() => props.selectField()}
-    >
-      <div className={props.className}></div>
-      {chips}
-    </section>
-  );
 };
 
 export default Field;
